8aee25aafdb93087725c4b0fa13aa960de93fc32
tweneboah <twentekghana@gmail.com>
2019-03-28 14:02:44 +0800

    CRUD Using Express

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = new  express.Router()
-const User = require('../src/model/users')
+const User  = require('../src/model/users')
 
 
 //Creating user/POST
